34ea2654c65cd889577949091f5ad3327952b637
jmutton2 <jmutton2@uwo.ca>
2021-08-02 12:44:46 -0400

    started creating the layout for the app, need to figure out a way to allow for individually row horizontal scrolling. App bar is fine but missing some icons.

--- client/cloudflix/src/App.js
+++ client/cloudflix/src/App.js
@@ -1,14 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import { Container, Grid, Paper, Typography } from '@material-ui/core';
+import AppHeader from './components/Header/AppHeader.js'
+import { makeStyles } from '@material-ui/core/styles';
 
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
 const App = () => {
+
+  const classes = useStyles();
+
+
   return(
     <Container>
-      
+        <AppHeader/>
+      <Grid>
+        <Grid container className={classes.root} spacing={2}>
+          <Grid item xs={12}>
+            <Grid container spacing={2}>
+              {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((value) => (
+                <Grid key={value} item>
+                  <Paper className={classes.paper} />
+                </Grid>
+              ))}
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid container className={classes.root} spacing={2}>
+          <Grid item xs={12}>
+            <Grid container spacing={2}>
+              {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((value) => (
+                <Grid key={value} item>
+                  <Paper className={classes.paper} />
+                </Grid>
+              ))}
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid container className={classes.root} spacing={2}>
+          <Grid item xs={12}>
+            <Grid container spacing={2}>
+              {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((value) => (
+                <Grid key={value} item>
+                  <Paper className={classes.paper} />
+                </Grid>
+              ))}
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     </Container>
   );
-}
 
+}
 export default App;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    width: '200%'
+  },
+  paper: {
+    height: 140,
+    width: 100,
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
+}));
+
